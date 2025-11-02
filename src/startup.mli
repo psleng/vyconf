8bf64063061e74e0e98c6ea904c6b1d58fcc0634
John Estabrook <jestabro@vyos.io>
2025-11-01 22:19:34 -0500

    T7980: add function load_config_cache

--- src/startup.mli
+++ src/startup.mli
@@ -14,6 +14,8 @@ val create_server :
     (Lwt_unix.file_descr * Lwt_unix.sockaddr -> unit Lwt.t) ->
     Lwt_unix.file_descr -> unit -> 'a Lwt.t
 
+val load_config_cache : string -> (Vyos1x.Config_tree.t, string) result
+
 val load_config_failsafe : string -> string -> Vyos1x.Config_tree.t
 
 val load_interface_definitions : string -> (Vyos1x.Reference_tree.t, string) result
